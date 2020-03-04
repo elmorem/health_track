adda30d7131fdcb3d885c4fba5c61bafade0c0da
elmorem <elmorem@gmail.com>
2020-03-04 05:46:38 -0800

    postgres name

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import flask_SQLAlchemy
+
 
 basedir=os.path.abspath(os.path.dirname(__file__))
-print(basedir)
 
 
-postgresql://mark:elmorem@localhost/ht_db
+
+app.config['SQLALCHEMY_DATABASE_URI'] = postgresql://mark:elmorem@localhost/ht_db
 
